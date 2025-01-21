78e3a6bfd5c955121d5374171bb59f73cbafca64
Christian Rozal <christian@alexium.com.au>
2025-01-21 11:38:29 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { Button } from "@heroui/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+
+  return (
+    <div className="w-sreen h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-5">
+        <Button
+          onPress={() => router.push("/dealership/capital-toyota/signup")}
+          className="bg-color1 font-semibold text-white"
+        >
+          Sign Up
+        </Button>
+        <Button
+          onPress={() => router.push("/consultant/adfsjfhas")}
+          className="bg-color1 font-semibold text-white"
+        >
+          Consultant
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
