74856c0ad574c27a936d6814a4272f73efe64898
Christian Rozal <christian@alexium.com.au>
2025-01-21 12:22:56 +0800

    commit

--- src/app/consultant/[slug]/page.tsx
+++ src/app/consultant/[slug]/page.tsx
@@ -1,8 +1,36 @@
+"use client";
 import { Button } from "@heroui/react";
 import Image from "next/image";
 import React from "react";
 
 const ConsultantSlugPage = () => {
+  const generateVCard = () => {
+    const vCardData = `BEGIN:VCARD
+VERSION:3.0
+FN:Alex Bompane
+N:Bompane;Alex;;;
+ORG:Alexium
+TITLE:Sales Consultant
+EMAIL:abompane@alexium.com.au
+TEL:(03) 9847 7927
+URL:www.alexium.com.au
+END:VCARD`;
+    return vCardData;
+  };
+  const handleSaveContact = () => {
+    const vCardData = generateVCard();
+    const blob = new Blob([vCardData], { type: "text/vcard" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "Alex Bompane.vcf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link); // Clean up the element after download.
+    URL.revokeObjectURL(url); // Revoke the URL after download.
+  };
+
   return (
     <div className="border max-w-sm min-h-screen mx-auto py-4 px-10">
       <Image
@@ -62,7 +90,7 @@ const ConsultantSlugPage = () => {
             height={17}
             alt="Phone Icon"
           />
-          <p>&#40;03&#41; 9847 7927</p>
+          <p>(03) 9847 7927</p>
         </div>
         <div className="flex gap-4 items-center py-3 rounded-md bg-color3 px-5">
           <Image
@@ -76,7 +104,10 @@ const ConsultantSlugPage = () => {
       </div>
 
       <div className="mt-10">
-        <Button className="bg-color1 font-semibold text-sm rounded-full w-full text-white">
+        <Button
+          className="bg-color1 font-semibold text-sm rounded-full w-full text-white"
+          onPress={handleSaveContact}
+        >
           Save Contact
         </Button>
         <Button className="bg-color3 font-semibold text-sm rounded-full w-full text-color1 mt-5">
