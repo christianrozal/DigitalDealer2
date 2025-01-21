288296194aacb5ffa7989fbc38fde245474df63c
Christian Rozal <christian@alexium.com.au>
2025-01-22 01:54:18 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@
 import { Button } from "@heroui/react";
 import { useRouter } from "next/navigation";
 import React from "react";
-
-// commit
+import { account } from "./lib/appwrite";
 
 const Home = () => {
   const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await account.deleteSession("current");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
 
   return (
     <div className="w-sreen h-screen flex items-center justify-center">
@@ -19,10 +26,17 @@ const Home = () => {
           Sign Up
         </Button>
         <Button
-          onPress={() => router.push("/consultant/adfsjfhas")}
+          onPress={() => router.push("/consultant/678def9400177e527a26")}
           className="bg-color1 font-semibold text-white"
         >
           Consultant
+        </Button>
+
+        <Button
+          onPress={handleLogout}
+          className="bg-color1 font-semibold text-white"
+        >
+          Logout
         </Button>
       </div>
     </div>
